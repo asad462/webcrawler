b237996960e1d2dab84f8a71813865f1abf8cb31
asad462 <mirasaduddinadil@gmail.com>
2023-09-03 16:48:17 +0530

    Did some tests on the URLS and added a jsdom dependency.

--- crawl.js
+++ crawl.js
@@ -1,7 +1,26 @@
+function getURLfromHTML(HTMLbody, baseURL){
+    const urls = []
+    return urls
+}
+
+
+
+
 function normalizeURL(urlString){
-    return urlString
+    /* new URL() gives the URL Object. */
+    /* We are parsing the URL String in to an object. */  
+    const urlObj = new URL(urlString)
+
+    /* Return the hostname and the pathname in the form of a string. */
+    const hostPath = urlObj.hostname + urlObj.pathname
+    /* Taking care of trailing slashes. */
+    if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
+        return hostPath.slice(0,-1)
+    }
+    return hostPath
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,3 +1,25 @@
-const { normalizeURL } = require('.crawl.js')
+const { normalizeURL, getURLfromHTML } = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
+/* To find out what page we are in, it is important to know the domain name and the path of the URL. So, we strip the protocol off the URL and compare the string.  */
+test('normalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL capitals', () => {
+    const input = 'http://blog.BOOT.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
